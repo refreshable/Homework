6fd28fa09c5c9cd5384628e20112f5ec03429c37
Gergana Krumova <extrim_g@abv.bg>
2017-05-31 09:48:07 +0300

    08. Calories Counter

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int kcal = 0;
+        string product = "";
+        for (int i = 0; i < n; i++)
+        {
+            product = Console.ReadLine().ToLower();
+            switch (product)
+            {
+                case "cheese": kcal += 500; break;
+                case "tomato sauce": kcal += 150; break;
+                case "salami": kcal += 600; break;
+                case "pepper": kcal += 50; break;
+
+                default:
+                    continue;
+            }
+        }
+        Console.WriteLine($"Total calories: {kcal}");
+    }
+}
